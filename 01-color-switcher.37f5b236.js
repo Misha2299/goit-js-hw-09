6c5b2c83a7b98fdef3bfc35039a9497f82dad707
Misha2299 <130393993+Misha2299@users.noreply.github.com>
2023-11-30 17:19:19 +0000

    Deploying to gh-pages from @ Misha2299/goit-js-hw-09@c38b27f793881dc7b4fb4026f49646585abbb8b9 🚀

---
+++ 01-color-switcher.37f5b236.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(a){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(a){t.disabled=!1,e.disabled=!0,clearInterval(n)})),e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.37f5b236.js.map
